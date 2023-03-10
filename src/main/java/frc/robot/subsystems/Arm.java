7df410db09e032e91b671f00b470df5d5be84cb3
SentinelAssault <94572905+SentinelAssault@users.noreply.github.com>
2023-03-10 01:09:24 -0600

    Changed to Appropriate motor controllers

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
 public class Arm {
 
-    CANSparkMax 
-    CANSparkMax
+    CANSparkMax jointMotor;
+    VictorSPX intakeMotor;
 
     public Arm(int jointMotorID, int intakeMotorID) {
         
--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -8,21 +9,20 @@ import frc.robot.Constants;
 public class GroundIntake {
 
     CANSparkMax jointMotor;
-    CANSparkMax rollerMotor;
+    VictorSPX rollerMotor;
 
     public GroundIntake(int jointMotorID, int rollerMotorID) {
         jointMotor = new CANSparkMax(jointMotorID, MotorType.kBrushless);
-        rollerMotor = new CANSparkMax(rollerMotorID, MotorType.kBrushless);
+        rollerMotor = new VictorSPX(rollerMotorID);
 
         jointMotor.setSmartCurrentLimit(Constants.GroundIntake.gndJointMotorCurrentLimit);
-        rollerMotor.setSmartCurrentLimit(Constants.GroundIntake.gndRollerMotorCurrentLimit);
     }
 
     public CANSparkMax getJointMotor() {
         return jointMotor;
     }
 
-    public CANSparkMax getRollerMotor() {
+    public VictorSPX getRollerMotor() {
         return rollerMotor;
     }
 }
